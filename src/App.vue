69f90ce200bfdf9a5f0754f3b532258f8d10992f
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-09 17:55:51 -0800

    Initial clean up

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <DatGui />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import DatGui from './components/DatGui.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    DatGui
   }
 });
 </script>
@@ -20,8 +19,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/DatGui.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+      {{ name }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { name } from './controls/label/index';
+
+export default defineComponent({
+    setup() {
+        return {
+            name
+        }
+    }
+});
+</script>
+
+<style lang="scss">
+    .container {
+        background-color: red;
+    }
+</style>
