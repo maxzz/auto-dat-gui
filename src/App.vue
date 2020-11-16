718460c3b88db0048aefe8f866df39ed16008561
Max Zakharzhevskiy <maxzz@msn.com>
2020-11-15 20:22:28 -0800

    Refactor: DatGui.vue w/ slot

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,95 @@
 <template>
-    <DatGui />
+    <DatGui>
+        <RowTitle label="Title" background="olivedrab" color="#f5dd05" />
+        <!-- <RowTitle label="Title" :background="testColor" :color="testColor" /> -->
+
+        <RowColor label="Color" v-model:color="testColor" color="#f29305" />
+        <RowColor label="Color" v-model:color="testColor" />
+        <!-- <RowColor label="Color" v-model:color="testColor" /> -->
+
+        <RowSelect label="Select" :items="testItems" v-model:value="testSelect" />
+
+        <RowBoolean v-model:checked="testBoolean" label="Boolean" title="I can explain that" />
+        <RowString v-model:value="testString" label="Text" title="... or not" />
+
+        <RowNumber v-model:value="testNumber" label="Number slider slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+        <RowNumber v-model:value="testNumber" label="Number" />
+
+        <RowButton label="Button" title="This handler invoked with isTrusted = true" @clicked="buttonClicked" />
+
+        <RowFolder label="Folder" closed>
+            <!-- <RowColor label="Color" v-model:color="testColor" /> -->
+
+            <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+            <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+
+            <RowFolder label="Nested Folder" closed>
+                <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+                <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+
+                <RowFolder label="Nested Folder" closed>
+                    <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+                    <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
+                </RowFolder>
+            </RowFolder>
+        </RowFolder>
+    </DatGui>
+<div class="results">
+<pre>
+Test results:
+    Number : {{testNumber}}
+    Boolean: {{testBoolean}}
+    Text   : {{testString}}
+    Select : {{testSelect}} -> {{findSelectedValue()}}
+    Color  : {{testColor}}
+</pre>
+    <span class="results-color" :style="{ color: color4Background(testColor), 'background-color': testColor }" >&nbsp;&nbsp;{{testColor}}&nbsp;&nbsp;</span>
+</div>
+
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue";
+    import { defineComponent, reactive, ref, toRefs } from "vue";
     import DatGui from "./components/DatGui.vue";
+    import RowNumber from "./components/RowNumber.vue";
+    import RowBoolean from "./components/RowBoolean.vue";
+    import RowString from "./components/RowString.vue";
+    import RowButton from "./components/RowButton.vue";
+    import RowFolder from "./components/RowFolder.vue";
+    import RowSelect from "./components/RowSelect.vue";
+    import RowTitle from "./components/RowTitle.vue";
+    import RowColor from "./components/RowColor.vue";
+    import { color4Background } from "./utils/colors";
 
     export default defineComponent({
         name: "App",
-        components: {
-            DatGui,
-        },
+        components: { DatGui,RowNumber, RowBoolean, RowString, RowButton, RowFolder, RowSelect, RowTitle, RowColor },
+        setup() {
+            const state = reactive({
+                testNumber: 70,
+                testBoolean: true,
+                testString: 'ABC',
+                testSelect: 'one',
+                testItems: [{name: 'First option', value: 'one' }, {name: 'Second option', value: 'two' }],
+                testColor: '',
+            });
+
+            function buttonClicked(evt: MouseEvent) {
+                console.log(`buttonClicked: What to do with trusted(${evt.isTrusted}) click event`, evt);
+            }
+
+            function findSelectedValue() {
+                let item = state.testItems.find(_ => _.value === state.testSelect);
+                return item?.name || 'none';
+            }
+
+            return {
+                ...toRefs(state),
+                buttonClicked,
+                findSelectedValue,
+                color4Background,
+            };
+        }
     });
 </script>
 
--- src/components/DatGui.vue
+++ src/components/DatGui.vue
@@ -2,100 +2,22 @@
     <div :class="['dat-gui', { closed: folded }]">
         <div class="group--main group">
             <ul>
-                <RowTitle label="Title" background="olivedrab" color="#f5dd05" />
-                <!-- <RowTitle label="Title" :background="testColor" :color="testColor" /> -->
-
-                <RowColor label="Color" v-model:color="testColor" color="#f29305" />
-                <RowColor label="Color" v-model:color="testColor" />
-                <!-- <RowColor label="Color" v-model:color="testColor" /> -->
-
-                <RowSelect label="Select" :items="testItems" v-model:value="testSelect" />
-
-                <RowBoolean v-model:checked="testBoolean" label="Boolean" title="I can explain that" />
-                <RowString v-model:value="testString" label="Text" title="... or not" />
-
-                <RowNumber v-model:value="testNumber" label="Number slider slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-                <RowNumber v-model:value="testNumber" label="Number" />
-
-                <RowButton label="Button" title="This handler invoked with isTrusted = true" @clicked="buttonClicked" />
-
-                <RowFolder label="Folder" closed>
-                    <!-- <RowColor label="Color" v-model:color="testColor" /> -->
-
-                    <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-                    <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-
-                    <RowFolder label="Nested Folder" closed>
-                        <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-                        <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-
-                        <RowFolder label="Nested Folder" closed>
-                            <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-                            <RowNumber v-model:value="testNumber" label="Number slider" :min="-100" :max="100" :step="1" title="Here is how it works" />
-                        </RowFolder>
-                    </RowFolder>
-                </RowFolder>
+                <slot></slot>
             </ul>
         </div>
         <div class="toggle-button" @click="folded=!folded">Show controls</div>
     </div>
-
-<div class="results">
-<pre>
-Test results:
-    Number : {{testNumber}}
-    Boolean: {{testBoolean}}
-    Text   : {{testString}}
-    Select : {{testSelect}} -> {{findSelectedValue()}}
-    Color  : {{testColor}}
-</pre>
-    <span class="results-color" :style="{ color: color4Background(testColor), 'background-color': testColor }" >&nbsp;&nbsp;{{testColor}}&nbsp;&nbsp;</span>
-</div>
-
 </template>
 
 <script lang="ts">
-    import { defineComponent, ref, reactive, toRefs } from 'vue';
-    import { color4Background } from "../utils/colors";
-    import RowNumber from "./RowNumber.vue";
-    import RowBoolean from "./RowBoolean.vue";
-    import RowString from "./RowString.vue";
-    import RowButton from "./RowButton.vue";
-    import RowFolder from "./RowFolder.vue";
-    import RowSelect from "./RowSelect.vue";
-    import RowTitle from "./RowTitle.vue";
-    import RowColor from "./RowColor.vue";
+    import { defineComponent, ref } from 'vue';
 
     export default defineComponent({
-        components: { RowNumber, RowBoolean, RowString, RowButton, RowFolder, RowSelect, RowTitle, RowColor },
         setup() {
-            const state = reactive({
-                testNumber: 70,
-                testBoolean: true,
-                testString: 'ABC',
-                testSelect: 'one',
-                testItems: [{name: 'First option', value: 'one' }, {name: 'Second option', value: 'two' }],
-                testColor: '',
-            });
-
             const folded = ref(false);
-
-            function buttonClicked(evt: MouseEvent) {
-                console.log(`buttonClicked: What to do with trusted(${evt.isTrusted}) click event`, evt);
-            }
-
-            function findSelectedValue() {
-                let item = state.testItems.find(_ => _.value === state.testSelect);
-                return item?.name || 'none';
-            }
-
             return {
                 folded,
-                ...toRefs(state),
-                buttonClicked,
-                findSelectedValue,
-                color4Background,
-            }
+            };
         }
     });
 </script>
